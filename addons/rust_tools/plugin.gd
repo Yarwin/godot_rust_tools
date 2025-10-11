c76810a6217bf9b8ef20afe15bd13ab7c9da2040
Iride Lombardi <lorenzomassidda@hotmail.com>
2025-10-11 19:02:05 +0200

    feat: make automatic build optional (#25)

--- addons/rust_tools/plugin.gd
+++ addons/rust_tools/plugin.gd
@@ -27,6 +27,10 @@ func _exit_tree() -> void:
 
 
 func _build() -> bool:
+	var build_before_run := RustToolsSettings.get_enable_build_before_run()
+	if not build_before_run:
+		print_rich("  Godot Rust Tools: [b][color=green]Skipping build before running[/color][/b]")
+		return true
 	return _build_sync()
 
 
